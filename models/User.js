52e8fc8345ebbfcdb36d53d56f3c0859e621a4be
quintonmills <quintonmills98@gmail.com>
2025-06-26 12:23:30 -0400

    setup auth service

---
+++ models/User.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, unique: true },
+  password: String,
+  name: String,
+  avatar: String,
+  googleId: String,
+  facebookId: String,
+  appleId: String,
+  createdAt: { type: Date, default: Date.now }
+});
+
+// Hash password before saving
+userSchema.pre('save', async function(next) {
+  if (!this.isModified('password')) return next();
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+module.exports = mongoose.model('User', userSchema);
